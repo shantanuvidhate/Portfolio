ccda00313b51914afc71621e9d17caab791adb1a
shantanu <shantanuvidhate00@gmail.com>
2022-10-18 12:21:32 +0530

    Profile Added to Home Component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import "./home.css";
+import Social from './Social';
+import Data from './Data';
+import ScrollDown from './ScrollDown'
 
 const Home = () => {
   return (
     <section className="home section" id="home">
         <div className="home__container container grid">
-            <div className="home__content grid"></div>
+            <div className="home__content grid">
+              <Social />
+
+              <div className="home__image"></div>
+              <Data />
+            </div>
+            <ScrollDown />
         </div>
     </section>
   )
--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import RedditIcon from '@mui/icons-material/Reddit';
 
 const Social = () => {
   return (
-    
+    <div className="home__social">
+      <a href="https://www.linkedin.com" className="home__social-icon" target="_blank" rel="noreferrer">
+      <LinkedInIcon />
+      </a>
+      
+      <a href="https://github.com/" className="home__social-icon" target="_blank" rel="noreferrer">
+      <GitHubIcon />
+      </a>
+
+      <a href="https://www.reddit.com/" className="home__social-icon" target="_blank" rel="noreferrer">
+      <RedditIcon />
+
+      </a>
+    </div>
   )
 }
 
